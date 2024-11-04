ece6251a08839e4691eaed22ce98d1ad87ef370d
Pranaymaurya <139427478+Pranaymaurya@users.noreply.github.com>
2024-11-04 20:15:26 +0530

    components made

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import PetCareSection from '../components/Petcare'
+import PetServices from '../components/Petservice'
+import BenefitSection from '../components/Benefit'
 
 const Home = () => {
   return (
     <div>
 Home page
+<PetCareSection/>
+<PetServices/>
+<BenefitSection/>
     </div>
   )
 }
